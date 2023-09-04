67956cf42779b5afd25290d499e0965a40351522
objektwerks <objektwerks@runbox.com>
2023-09-04 16:56:59 -0400

    edited scaladoc

--- src/main/scala/objektwerks/A95.scala
+++ src/main/scala/objektwerks/A95.scala
@@ -3,7 +3,7 @@ package objektwerks
 import scala.annotation.tailrec
 
 /**
-  * Does string contain balanced parens?
+  * Does a string contain balanced parens?
   */
 object A95:
   def containsBalancedParens(string: String): Boolean =
