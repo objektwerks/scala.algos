2ec56caed6546878ecaa81fbda07e76f1491a78f
objektwerks <objektwerks@runbox.com>
2023-09-04 10:11:12 -0400

    added algo 95

---
+++ src/main/scala/objektwerks/A95.scala
@@ -0,0 +1,17 @@
+package objektwerks
+
+import scala.annotation.tailrec
+
+/**
+  * Does string contain balanced parens?
+  */
+object A95:
+  def containsBalancedParens(string: String): Boolean =
+    @tailrec
+    def loop(remaining: String, openParens: Int): Boolean =
+      if remaining.isEmpty then openParens == 0
+      else if openParens == 0 && remaining.head == ')' then false
+      else if remaining.head == '(' then loop(remaining.tail, openParens + 1)
+      else loop(remaining.tail, openParens - 1)
+
+    loop(string, 0)
