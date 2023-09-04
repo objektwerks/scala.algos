df773fe90a472fa7a8ff80689fefe20eca38a927
objektwerks <objektwerks@runbox.com>
2023-09-04 10:40:39 -0400

    added algo 98 test

---
+++ src/test/scala/objektwerks/A98Test.scala
@@ -0,0 +1,11 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import A98.*
+
+class A98Test extends AnyFunSuite with Matchers:
+  test("max seq of zeroes") {
+
+  }
