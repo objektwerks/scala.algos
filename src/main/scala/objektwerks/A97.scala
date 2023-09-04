2c61a68e7370490e6516573d8b8280330b26f436
objektwerks <objektwerks@runbox.com>
2023-09-04 09:44:56 -0400

    added algo 97

---
+++ src/main/scala/objektwerks/A97.scala
@@ -0,0 +1,13 @@
+package objektwerks
+
+/**
+  * Find an unpaired number in a list.
+  */
+object A97:
+  def findUnpairedItem[T](list: List[T]): Option[T] =
+    list
+      .foldLeft[Set[T]](Set.empty) {
+        case (set, t) if set.contains(t) => set - t
+        case (set, t) => set + t
+      }
+      .headOption
