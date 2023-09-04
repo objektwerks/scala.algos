4d2fe6db29cc65b356541c7efaae1b09ff9a4499
objektwerks <objektwerks@runbox.com>
2023-09-04 09:01:09 -0400

    added algo 96 test

---
+++ src/test/scala/objektwerks/A96Test.scala
@@ -0,0 +1,12 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import A96.* 
+
+class A96Test extends AnyFunSuite with Matchers:
+  test("palindrome") {
+    isPalindrome( List('a', 'a', 'b', 'b')) shouldBe false
+    isPalindrome( List('a', 'b', 'a')) shouldBe true
+  }
