51ca68917676c288fb6efd4e695dcd8aef964b67
objektwerks <objektwerks@runbox.com>
2023-09-04 08:51:42 -0400

    added algo 100

---
+++ src/main/scala/objektwerks/A100.scala
@@ -0,0 +1,16 @@
+package objektwerks
+
+/**
+  * For every number input, you should be able to output:
+  * 1. if divisible by 15 ( 3 * 5 ) return "fizzbuzz"
+  * 2. else if divisible by 3 return "fizz"
+  * 3. else if divisible by 5 return "buzz"
+  * 4. else return input number
+  */
+object A100:
+  def fizzbuzz(n: Int): String =
+    n match 
+      case i if i % 15 == 0 => "fizzbuzz"
+      case j if j % 3 == 0 => "fizz"
+      case k if k % 5 == 0 => "buzz"
+      case _ => s"$n"
