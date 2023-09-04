1d777c51f2af1cba237bf64604a43a26ce240719
objektwerks <objektwerks@runbox.com>
2023-09-04 10:26:41 -0400

    enhanced algo 98

--- src/main/scala/objektwerks/A98.scala
+++ src/main/scala/objektwerks/A98.scala
@@ -1,6 +1,17 @@
 package objektwerks
 
+import scala.annotation.tailrec
+
+/**
+  * Does string contain balanced parens?
+  */
 object A98:
   def isEven(n: Int): Boolean = !isOdd(n)
 
   def isOdd(n: Int): Boolean = (n & 1) == 1
+
+  @tailrec
+  def removeRightZeroes(number: Int): Int =
+    if (number == 0) then number
+    else if (isOdd(number)) then number
+    else removeRightZeroes(number >> 1)
