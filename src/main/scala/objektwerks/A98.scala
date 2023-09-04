9ad420c0e0dca26464a2aeea2feb03eec3179960
objektwerks <objektwerks@runbox.com>
2023-09-04 10:30:52 -0400

    enhanced algo 98

--- src/main/scala/objektwerks/A98.scala
+++ src/main/scala/objektwerks/A98.scala
@@ -3,7 +3,7 @@ package objektwerks
 import scala.annotation.tailrec
 
 /**
-  * Does string contain balanced parens?
+  * Find longest sequence of zeros in binary representation of an integer.
   */
 object A98:
   def isEven(n: Int): Boolean = !isOdd(n)
@@ -15,3 +15,18 @@ object A98:
     if (number == 0) then number
     else if (isOdd(number)) then number
     else removeRightZeroes(number >> 1)
+
+  def maxBinaryGap(n: Int): Option[Int] =
+    if (n <= 0) then None
+    else
+      val numberStartingWith1 = removeRightZeroes(n)
+
+      @tailrec
+      def iterate(currentNumber: Int,
+                  currentCount: Int,
+                  gapCounts: List[Int]): Option[Int] =
+      if (currentNumber == 1) then (currentCount :: gapCounts).maxOption.filter(_ > 0)
+      else if (isEven(currentNumber)) then iterate(currentNumber >> 1, currentCount + 1, gapCounts)
+      else iterate(currentNumber >> 1, 0, currentCount :: gapCounts)
+      
+      iterate(numberStartingWith1, 0, Nil)
