e8358a17aa01b80ca67ec508097b5826cd994ed2
objektwerks <objektwerks@runbox.com>
2023-09-04 10:47:54 -0400

    enhanced algo 98 test

--- src/test/scala/objektwerks/A98Test.scala
+++ src/test/scala/objektwerks/A98Test.scala
@@ -7,5 +7,8 @@ import A98.*
 
 class A98Test extends AnyFunSuite with Matchers:
   test("max seq of zeroes") {
-
+    removeRightZeroes(12) shouldBe 3
+    maxSeqOfZeroes(6217).contains(4)
+    maxSeqOfZeroes(16).isEmpty
+    maxSeqOfZeroes(1).isEmpty
   }
