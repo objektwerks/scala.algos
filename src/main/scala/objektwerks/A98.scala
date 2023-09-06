f26fca7ef99862ffd5ccc0b73d78dcb9b879c893
objektwerks <objektwerks@runbox.com>
2023-09-06 10:46:12 -0400

    edited algo 98

--- src/main/scala/objektwerks/A98.scala
+++ src/main/scala/objektwerks/A98.scala
@@ -24,9 +24,9 @@ object A98:
       @tailrec
       def loop(currentNumber: Int,
                currentCount: Int,
-               binaryGapCounts: List[Int]): Option[Int] =
-      if currentNumber == 1 then (currentCount :: binaryGapCounts).maxOption.filter(_ > 0)
-      else if isEven(currentNumber) then loop(currentNumber >> 1, currentCount + 1, binaryGapCounts)
-      else loop(currentNumber >> 1, 0, currentCount :: binaryGapCounts)
+               zeroGapCounts: List[Int]): Option[Int] =
+      if currentNumber == 1 then (currentCount :: zeroGapCounts).maxOption.filter(_ > 0)
+      else if isEven(currentNumber) then loop(currentNumber >> 1, currentCount + 1, zeroGapCounts)
+      else loop(currentNumber >> 1, 0, currentCount :: zeroGapCounts)
       
       loop(numberStartingWith1, 0, Nil)
