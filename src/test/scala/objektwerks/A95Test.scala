7a7d9bb9018b99cd19f23d0b233cae9197176f66
objektwerks <objektwerks@runbox.com>
2023-09-21 05:49:18 -0400

    moved a 95 to a 95 test

--- src/test/scala/objektwerks/A95Test.scala
+++ src/test/scala/objektwerks/A95Test.scala
@@ -3,6 +3,22 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import scala.annotation.tailrec
+
+/**
+  * Assert a String contains balanced parens.
+  */
+object A95:
+  def containsBalancedParens(string: String): Boolean =
+    @tailrec
+    def loop(remaining: String, openParens: Int): Boolean =
+      if remaining.isEmpty then openParens == 0
+      else if openParens == 0 && remaining.head == ')' then false
+      else if remaining.head == '(' then loop(remaining.tail, openParens + 1)
+      else loop(remaining.tail, openParens - 1)
+
+    loop(string, 0)
+
 import A95.*
 
 class A95Test extends AnyFunSuite with Matchers:
