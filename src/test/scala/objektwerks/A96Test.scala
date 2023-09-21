8879a164acc0fe3a6e9f5f18670c0f6492080d02
objektwerks <objektwerks@runbox.com>
2023-09-21 05:50:16 -0400

    added a 96 to a 96 test

--- src/test/scala/objektwerks/A96Test.scala
+++ src/test/scala/objektwerks/A96Test.scala
@@ -3,6 +3,19 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+/**
+  * A palindrome is a sequence of items that are identical when reversed.
+  * For instance, 'aabb' is not a palindrome. But 'aba' is a palindrome.
+  * 
+  * Assert a list of T contains a palidrome.
+  */
+object A96:
+  def isPalindrome[T](list: List[T]): Boolean =
+    list
+      .view
+      .zip(list.reverse)
+      .forall { case (a, b) => a == b }
+
 import A96.* 
 
 class A96Test extends AnyFunSuite with Matchers:
