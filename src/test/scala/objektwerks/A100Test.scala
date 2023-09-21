32bed0d579b70af66246bc271b2e78b5a6790b25
objektwerks <objektwerks@runbox.com>
2023-09-21 05:54:16 -0400

    moved a 100 to a 100 test

--- src/test/scala/objektwerks/A100Test.scala
+++ src/test/scala/objektwerks/A100Test.scala
@@ -3,6 +3,21 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+/**
+  * For an Int, return a String of:
+  * 1. if int divisible by 15 ( 3 * 5 ) return "fizzbuzz"
+  * 2. else if int divisible by 3 return "fizz"
+  * 3. else if int divisible by 5 return "buzz"
+  * 4. else return int
+  */
+object A100:
+  def fizzbuzz(n: Int): String =
+    n match 
+      case i if i % 15 == 0 => "fizzbuzz"
+      case j if j % 3 == 0 => "fizz"
+      case k if k % 5 == 0 => "buzz"
+      case _ => s"$n"
+
 import A100.*
 
 class A100Test extends AnyFunSuite with Matchers:
