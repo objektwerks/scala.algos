a0b891e4162d78d57cb9847779bc1f76ad81f624
objektwerks <objektwerks@runbox.com>
2023-09-24 08:16:45 -0400

    reformatted test

--- src/test/scala/objektwerks/A97Test.scala
+++ src/test/scala/objektwerks/A97Test.scala
@@ -18,12 +18,11 @@ object A97:
 
 import A97.*
 
-class A97Test extends AnyFunSuite with Matchers:
-  test("find unpaired item") {
+final class A97Test extends AnyFunSuite with Matchers:
+  test("find unpaired item"):
     findUnpairedItem(List(1, 2, 1)).get shouldBe 2
     findUnpairedItem(List(1, 2, 1, 1, 1)).get shouldBe 2
     findUnpairedItem(List(1, 1, 1, 1, 1)).get shouldBe 1
     findUnpairedItem(List(1, 1, 1, 1)).isEmpty shouldBe true
     findUnpairedItem(List(1, 2, 3, 1, 2)).get shouldBe 3
     findUnpairedItem(List(1, 2, 3, 3, 1, 2)).isEmpty shouldBe true
-  }
