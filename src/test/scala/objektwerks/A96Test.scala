34ae360b1cbdaf8ef4fa5d6590dd9be71dcc258e
objektwerks <objektwerks@runbox.com>
2023-09-24 08:16:21 -0400

    reformatted test

--- src/test/scala/objektwerks/A96Test.scala
+++ src/test/scala/objektwerks/A96Test.scala
@@ -18,8 +18,7 @@ object A96:
 
 import A96.* 
 
-class A96Test extends AnyFunSuite with Matchers:
-  test("is palindrome") {
+final class A96Test extends AnyFunSuite with Matchers:
+  test("is palindrome"):
     isPalindrome( List('a', 'a', 'b', 'b')) shouldBe false
     isPalindrome( List('a', 'b', 'a')) shouldBe true
-  }
