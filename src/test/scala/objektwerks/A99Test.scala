8114c91a0496ab5b67c99822c1046f15d7bbb963
objektwerks <objektwerks@runbox.com>
2023-09-24 08:17:30 -0400

    reformatted test

--- src/test/scala/objektwerks/A99Test.scala
+++ src/test/scala/objektwerks/A99Test.scala
@@ -27,10 +27,9 @@ object A99:
 
 import A99.*
 
-class A99Test extends AnyFunSuite with Matchers:
-  test("max profit") {
+final class A99Test extends AnyFunSuite with Matchers:
+  test("max profit"):
     maxProfit(List(163, 112, 105, 100, 151)).get shouldBe 51
     maxProfit(List(1)).isEmpty shouldBe true
     maxProfit(List(1, 2)).get shouldBe 1
     maxProfit(List(2, 1)).isEmpty shouldBe true
-  }
