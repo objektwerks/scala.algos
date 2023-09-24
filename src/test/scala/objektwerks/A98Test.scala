13a77c22c8627305d95396215478d623062cd02e
objektwerks <objektwerks@runbox.com>
2023-09-24 08:17:10 -0400

    reformatted test

--- src/test/scala/objektwerks/A98Test.scala
+++ src/test/scala/objektwerks/A98Test.scala
@@ -36,10 +36,9 @@ object A98:
 
 import A98.*
 
-class A98Test extends AnyFunSuite with Matchers:
-  test("max seq of zeroes") {
+final class A98Test extends AnyFunSuite with Matchers:
+  test("max seq of zeroes"):
     removeRightZeroes(12) shouldBe 3
     maxSeqOfZeroes(6217).contains(4)
     maxSeqOfZeroes(16).isEmpty
     maxSeqOfZeroes(1).isEmpty
-  }
